e5f65e379aca279510232db2c1c4e6846b377d05
Jay Bosamiya <jaybosamiya@gmail.com>
2015-10-15 10:33:02 +0530

    Add encryption and decryption to messages

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import threading
 import time
 import sys
 import network
+from diffie_hellman import DiffieHellman
+import crypto_protocol
 
 
 def usage():
@@ -16,16 +18,37 @@ if not (len(sys.argv) == 2 or len(sys.argv) == 3):
     usage()
 
 conn = network.Connection()
+dh = None
+
+def get_line():
+    line = None
+    while line is None:
+        line = conn.recv()
+    return line
 
 if len(sys.argv) == 2:  # server
     try:
         conn.listen(int(sys.argv[1]))
+        dh = DiffieHellman()
+        p, g, A = dh.generate_public_broadcast()
+        conn.send(str(p))
+        conn.send(str(g))
+        conn.send(str(A))
+        B = int(get_line())
+        crypto_protocol.init(dh.get_shared_secret(B))
     except:
         print "Unable to open port %d" % int(sys.argv[1])
         sys.exit()
 elif len(sys.argv) == 3:  # client
     try:
         conn.connect(sys.argv[1], int(sys.argv[2]))
+        p = int(get_line())
+        g = int(get_line())
+        A = int(get_line())
+        dh = DiffieHellman(p, g)
+        _, _, B = dh.generate_public_broadcast()
+        conn.send(str(B))
+        crypto_protocol.init(dh.get_shared_secret(A))
     except:
         print "Unable to connect to %s at port %d" % (sys.argv[1], int(sys.argv[2]))
         sys.exit()
@@ -35,7 +58,7 @@ else:
 
 
 def send_message(text):
-    conn.send(text)
+    conn.send(crypto_protocol.encrypt(text))
 
 
 class GUIThread (threading.Thread):
@@ -52,6 +75,6 @@ while True:
     if line is not None:
         import gui
         if line != '':
-            gui.add_new_text("[Other] " + line)
+            gui.add_new_text("[Other] " + crypto_protocol.decrypt(line))
     else:
         break
