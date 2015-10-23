9f07fc3a20b7f42ef51c6d5a354c9cdd08422abc
Jay Bosamiya <jaybosamiya@gmail.com>
2015-10-23 13:53:23 +0530

    Allow disabling input

--- gui.py
+++ gui.py
@@ -3,20 +3,14 @@
 import Tkinter
 import sys
 
-top = Tkinter.Tk()
-
-scrolling_text_box = Tkinter.Text(top, height=10)
-scrolling_text_box.pack(fill=Tkinter.X)
-
-input_text_box = Tkinter.Entry(top)
-input_text_box.pack(fill=Tkinter.X)
+message_sender_callback = None
+input_text_box = None
+scrolling_text_box = None
 
 
 def add_new_text(text):
     scrolling_text_box.insert(Tkinter.INSERT, text + "\n")
     scrolling_text_box.see(Tkinter.END)
-
-message_sender_callback = None
 
 
 def send_message_callback():
@@ -35,9 +29,19 @@ def set_send_message_callback(callback):
     global message_sender_callback
     message_sender_callback = callback
 
-Tkinter.Button(top, text="Send Message",
-               command=send_message_callback).pack()
 
+def start(disable_input=False):
+    global input_text_box, scrolling_text_box
 
-def start():
+    top = Tkinter.Tk()
+
+    scrolling_text_box = Tkinter.Text(top, height=10)
+    scrolling_text_box.pack(fill=Tkinter.X)
+
+    if not disable_input:
+        input_text_box = Tkinter.Entry(top)
+        input_text_box.pack(fill=Tkinter.X)
+        Tkinter.Button(top, text="Send Message",
+                       command=send_message_callback).pack()
+
     top.mainloop()
